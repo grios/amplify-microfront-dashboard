f0b11ef9cc4c1ed83004ee40070df06f6c2370c2
gibran.rios <gibran.rios@payclip.com>
2022-03-09 11:55:39 -0600

    Add amplify.yml file

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -7,7 +7,7 @@ const prodConfig = {
   mode: 'production',
   output: {
     filename: '[name].[contenthash].js',
-    publicPath: '/dashboard/latest/',
+    publicPath: '',
   },
   plugins: [
     new ModuleFederationPlugin({
